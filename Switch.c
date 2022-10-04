d7481481071e3bee4ee837ea6a992e723aaf3661
jayanivi30 <113873647+jayanivi30@users.noreply.github.com>
2022-10-05 00:25:38 +0530

    Create Switch.c

---
+++ Switch.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int main()
+{
+char grade;
+printf("Enter your grade:\n");
+scanf("%c",&grade);
+switch(grade)
+{
+case 'A':
+printf("Exellent and outstanding\n");
+break;
+case 'B':
+printf("Well done\n");
+break;
+case 'C':
+printf("keep it up");
+break;
+case 'D':
+printf("Do well next time");
+break;
+case 'F':
+printf("better luck next time");
+break;
+default:
+printf("Invalid grade");
+}
+return 0;
+}
