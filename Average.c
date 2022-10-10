a1d56f564cbd01fe76bd7bed03d522ffcee2cccc
Jayastri Nivedha <113873647+jayanivi30@users.noreply.github.com>
2022-10-10 22:32:20 +0530

    Create Average.c

---
+++ Average.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main()
+{
+    int n, i;
+    float sum = 0, x;
+
+    printf("Enter number of elements:  ");
+    scanf("%d", &n);
+    printf("Enter %d elements\n", n);
+    for(i = 0; i < n; i++)
+    {
+        scanf("%f", &x);
+        sum += x;
+    }
+    printf("Average of the entered numbers is =  %f", (sum/n));
+    return 0;
+}
