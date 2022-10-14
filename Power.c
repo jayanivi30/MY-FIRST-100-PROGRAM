07bedf8d81e842378a6c21001c61d994430dfbc5
Jayastri Nivedha <113873647+jayanivi30@users.noreply.github.com>
2022-10-14 20:39:33 +0530

    Update Power.c

--- Power.c
+++ Power.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 int main() {
+     printf("RA2211042010055");
     int base, exp, result;
     printf("Enter a base number: ");
     scanf("%d", &base);
     printf("Enter an exponent: ");
     scanf("%d", &exp);
     result = pow(base, exp);
-
     printf("%d^%d = %d", base, exp, result);
     return 0;
 }
