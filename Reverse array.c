279cb293df1230ccd6071bae0ac73f85aad51d8b
Jayastri Nivedha <113873647+jayanivi30@users.noreply.github.com>
2022-10-14 20:41:59 +0530

    Update Reverse array.c

--- Reverse array.c
+++ Reverse array.c
@@ -1,20 +1,17 @@
 #include<stdio.h>
 
 int main()
-{
-
+{ printf("RA2211042010055");
     int c, d, n, a[100], b[100];
     printf("Enter number of elements in array :\n");
     scanf("%d", &n);
     printf("Enter %d elements\n", n);
-
     for(c = 0; c < n; c++)
         scanf("%d", &a[c]);
     for(c = n-1, d = 0; c >= 0; c--, d++)
         b[d] = a[c];
     for(c = 0; c < n; c++)
-        a[c] = b[c];
-
+        a[c] = b[c]
     printf("Resultant array is: ");
     for(c = 0; c < n; c++)
         printf("%d", a[c]);
