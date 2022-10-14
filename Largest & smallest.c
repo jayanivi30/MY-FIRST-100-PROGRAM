6b04b9eb78a4de2a175f09b65d89a0b6ee18b8d1
Jayastri Nivedha <113873647+jayanivi30@users.noreply.github.com>
2022-10-14 20:33:44 +0530

    Update Largest & smallest.c

--- Largest & smallest.c
+++ Largest & smallest.c
@@ -3,12 +3,11 @@ include<stdio.h>
 int a,b;
 int main()
 {
+     printf("RA2211042010055");
     printf("Enter the two values to find the greatest and smallest number: \n");
     scanf("%d%d", &a, &b);
-
     if(a == b)
         printf("Both are equal\n");
-        
     else if(a < b)
     {
         printf("The largest number is %d\n", b);
